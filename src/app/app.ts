f767fff5a8e02e2a59aa4f3d78dc319b5dfd08a4
Arun Nedumbilly <arunnedumbilly@Naveenas-MacBook-Air.local>
2026-06-20 12:30:57 +0530

    Added GitHub Pages deployment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Cdirective } from './cdirective';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Cdirective],
+  imports: [Cdirective],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
